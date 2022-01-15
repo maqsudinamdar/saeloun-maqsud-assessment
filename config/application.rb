adfec1f00a850e13cd1a33f1d135a5f86cba02ca
maqsudinamdar <maqsudinamdar@gmail.com>
2022-01-15 15:52:15 +0530

    Initial Setup

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,13 @@ module Plate
       g.orm :active_record, primary_key_type: :uuid
     end
     config.active_job.queue_adapter = :sidekiq
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+    
   end
 end
